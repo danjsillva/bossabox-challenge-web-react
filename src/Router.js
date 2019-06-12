19a8ce393fc2ec013ed0544a32db86c393f94603
Daniel Silva <danielsilva@Daniels-MacBook-Air.local>
2019-06-11 21:11:15 -0300

    Removidos rotas e views desnecessarias

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Home from "./views/home/Home";
-import Login from "./views/login/Login";
-import Users from "./views/users/Users";
 import Tools from "./views/tools/Tools";
 
 export default function Router() {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/users" component={Users} />
-      <Route path="/tools" component={Tools} />
+      <Route path="/" exact component={Tools} />
     </Switch>
   );
 }
